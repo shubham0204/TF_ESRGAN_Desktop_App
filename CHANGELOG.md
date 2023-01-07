a35adb255faf423e6258352223ec733e91989031
shubham0204 <equipintelligence@gmail.com>
2023-01-07 10:00:26 +0530

    - Moved the loading of TF Hub model from main thread to a worker thread in `main.py` using `threading` - Fixed size of `QMainWindow`/`App` in `main.py` - *Super Resolve* button now shows `Processing...` when the image is being processed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
-- Added `pyinstaller` in `requirements.txt`
+- Moved the loading of TF Hub model from main thread to a 
+worker thread in `main.py` using `threading`
+- Fixed size of `QMainWindow`/`App` in `main.py`
+- *Super Resolve* button now shows `Processing...` when the 
+image is being processed.
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from PIL import ImageOps
 from PIL import ImageQt
 import PySide6.QtWidgets as widgets
 import PySide6.QtGui as gui
+import tensorflow_hub as hub
+import tensorflow as tf
 import numpy as np
 import os
 import sys
+import threading
 
-import tensorflow_hub as hub
-import tensorflow as tf
 
 class App( widgets.QMainWindow ):
 
@@ -18,16 +19,22 @@ class App( widgets.QMainWindow ):
         self.current_secs = None
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         self.setWindowTitle("Super Resolution with ESRGANs")
+        self.statusBar().setSizeGripEnabled(False)
+        self.setMaximumWidth( self.geometry().width())
+        self.setMaximumHeight( self.geometry().height())
 
         self.ui.save_image.clicked.connect(self.on_save_image_click)
         self.ui.select_image.clicked.connect(self.on_select_image_click)
         self.ui.super_resolve.clicked.connect(self.on_super_resolve_click)
 
-        self.model = hub.load( "https://tfhub.dev/captain-pool/esrgan-tf2/1" )
+        self.loading_thread = None
+        self.start_tf_model_loading()
 
         self.selected_image = None
         self.converted_image = None
+
 
     def on_select_image_click(self):
         selected_file_path = widgets.QFileDialog.getOpenFileName( self )
@@ -37,21 +44,32 @@ class App( widgets.QMainWindow ):
             self.set_image( image , self.ui.input_img )
             self.selected_image = np.asarray( image , dtype='float32' )
 
+
     def on_save_image_click(self):
         selected_file_path = widgets.QFileDialog.getExistingDirectory(self)
         if selected_file_path is not None:
             self.converted_image.save( os.path.join( selected_file_path , 'img.png' ) )
 
+
     def on_super_resolve_click(self):
+        if self.loading_thread.is_alive():
+            return
+
         low_resolution_image = np.expand_dims( self.selected_image , axis=0 )
         low_resolution_image = tf.cast( low_resolution_image , tf.float32 )
+
         self.ui.super_resolve.setText("Processing...")
+        self.ui.super_resolve.repaint()
+
         output = self.model( low_resolution_image ).numpy()[0]
         output = ( output - output.min() ) / ( output.max() - output.min() )
         output *= 255
         self.converted_image = Image.fromarray( output.astype( np.uint8 ) )
         self.set_image( self.converted_image , self.ui.output_img )
+
         self.ui.super_resolve.setText( "Super Resolve" )
+        self.ui.super_resolve.repaint()
+
 
     def set_image( self , image , label ):
         resized_image = ImageOps.contain( image , ( 500 , 500 ) )
@@ -59,7 +77,16 @@ class App( widgets.QMainWindow ):
         label.setPixmap(gui.QPixmap.fromImage(image))
 
 
+    def start_tf_model_loading(self):
+        self.loading_thread = threading.Thread( target=self.load_tf_model , name="TF Model Loading" )
+        self.loading_thread.start()
+
+    def load_tf_model( self ):
+        self.model = hub.load( "https://tfhub.dev/captain-pool/esrgan-tf2/1" )
+
+
 app = widgets.QApplication(sys.argv)
 window = App()
+
 window.show()
 sys.exit(app.exec())
